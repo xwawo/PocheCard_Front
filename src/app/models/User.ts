dee5b0c802adf4c1ad6263050dba1ee9c2637d6c
walid.iqbal <walid.iqbal@cpe.fr>
2023-05-26 17:33:46 +0200

    fix

---
+++ src/app/models/User.ts
@@ -0,0 +1,22 @@
+export class User {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    repeatPassword: string;
+
+    constructor(
+        username: string,
+        email: string,
+        firstName: string,
+        lastName: string,
+        password: string,
+    ) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../models/User";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+
+
+
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  private isLogged: boolean
+  user: User = new User('', '', '', '', ''); // Initialize a User object with empty strings
+  public repeatPassword: string;
+
+  constructor(
+      private http: HttpClient
+  ) {
+        this.isLogged = false;
+  }
 
   ngOnInit() {
   }
+
+  registerUser() {
+    const optionRequete = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin':'*',
+        'responseType': 'json'
+      })
+    }
+    const url = 'https://api.example.com/endpoint';
+    console.log(this.user);
+    if (
+        this.user.username &&
+        this.user.email &&
+        this.user.firstName &&
+        this.user.lastName &&
+        this.user.password
+    ) {
+      // Check if the passwords match
+      if (this.user.password === this.repeatPassword) {
+        // Registration logic
+        this.http.post<any[]>('http://localhost:8080/user',this.user, optionRequete).subscribe(
+            (response) => {
+              this.http.get('http://localhost:8080/user/all', optionRequete).subscribe(
+                    (response) => {
+                      console.log(response);
+                    }
+              );
+            }
+        )
+        // You can perform further actions like making an API call to register the user
+      } else {
+        console.log('Passwords do not match');
+        // Handle the case when passwords do not match
+      }
+    } else {
+      console.log('Please fill in all fields');
+      // Handle the case when any required field is empty
+    }
+  }
+    login() {
+
+        this.isLogged = true;
+    }
 
 }
